7cd3655f8e8590092eb75b4411442d1f5fd51c86
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2020-12-02 18:29:38 +0100

    milestone 2 - da rivedere le bandiere associate alle lingue

--- js/script.js
+++ js/script.js
@@ -2,15 +2,24 @@ var app = new Vue({
   el: '#app',
   data: {
     searchInput: '',
-    movies: []
+    movies: [],
+    series: []
   },
   methods: {
     search:function() {
       this.getMovies();
+      this.getSeries();
     },
     getMovies: function() {
         axios.get("https://api.themoviedb.org/3/search/movie?api_key=7cef99d6f2b7da5c519fa01d79fc87e1&query=" + this.searchInput)
           .then(result => this.movies = result.data.results )
     },
+    getSeries: function() {
+        axios.get("https://api.themoviedb.org/3/search/tv?api_key=7cef99d6f2b7da5c519fa01d79fc87e1&query=" + this.searchInput)
+          .then(result => this.series = result.data.results )
+    },
+    starRating: function(numRating) {
+      return Math.ceil(numRating / 2);
+    }
   }
 });
