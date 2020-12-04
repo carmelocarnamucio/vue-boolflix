0bafcd4f1e3e352b5b0fd3ad4836699aa224cbdc
carmelocaramucio <carnamuciocarmelo@gmail.com>
2020-12-04 17:32:44 +0100

    prova

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
+const urlmovie= 'https://api.themoviedb.org/3/search/movie'
+const urltv= 'https://api.themoviedb.org/3/search/tv'
+
 var app = new Vue({
   el: '#app',
   data: {
     searchInput: '',
     movies: [],
-    series: []
+    series: [],
+    moviesHome1: [],
+    moviesHome2: [],
+    pagesearch: '0',
+    query: '',
   },
+
+  mounted: function() {
+    this.home1();
+    this.home2();
+  },
+
   methods: {
     search:function() {
       this.getMovies();
@@ -20,6 +33,7 @@ var app = new Vue({
               }
     })
         .then(result => this.movies = result.data.results )
+        this.pagesearch = '1'
     },
     getSeries: function() {
       axios.get('https://api.themoviedb.org/3/search/tv', {
@@ -36,6 +50,28 @@ var app = new Vue({
     },
     setPosterPath: function(path) {
       return `https://image.tmdb.org/t/p/w342/${path}`;
-    }
+    },
+    home1: function() {
+      axios.get(urlmovie, {
+              params: {
+                api_key: '7cef99d6f2b7da5c519fa01d79fc87e1',
+                query: 'harry potter',
+                language: 'it-IT',
+              }
+    })
+        .then(result => this.moviesHome1 = result.data.results )
+
+    },
+    home2: function() {
+      axios.get(urlmovie, {
+              params: {
+                api_key: '7cef99d6f2b7da5c519fa01d79fc87e1',
+                query: 'il signore degli anelli',
+                language: 'it-IT',
+              }
+    })
+        .then(result => this.moviesHome2 = result.data.results )
+
+    },
   }
 });
