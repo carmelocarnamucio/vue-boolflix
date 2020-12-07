082a733ca7a5e993e7cf2d04b3b19d521ca18601
carmelocaramucio <carnamuciocarmelo@gmail.com>
2020-12-07 21:42:53 +0100

    miglioramenti vari

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const urlmovie= 'https://api.themoviedb.org/3/search/movie'
-const urltv= 'https://api.themoviedb.org/3/search/tv'
-
 var app = new Vue({
   el: '#app',
   data: {
@@ -10,7 +7,17 @@ var app = new Vue({
     moviesHome1: [],
     moviesHome2: [],
     pagesearch: '0',
+    queryHome1: 'Harry Potter',
+    queryHome2:'Il Signore Degli Anelli',
     query: '',
+    overlayStatus: false,
+    overlay: {
+      poster_path: '',
+      overview: '',
+      original_language: '',
+      vote_average: 0,
+      genre: [],
+    },
   },
 
   mounted: function() {
@@ -19,11 +26,14 @@ var app = new Vue({
   },
 
   methods: {
+    //funzione per la ricerca dei film e serie tv
     search:function() {
       this.getMovies();
       this.getSeries();
+      this.query = this.searchInput;
       this.searchInput = '';
     },
+
     getMovies: function() {
       axios.get('https://api.themoviedb.org/3/search/movie', {
               params: {
@@ -32,9 +42,10 @@ var app = new Vue({
                 language: 'it-IT',
               }
     })
-        .then(result => this.movies = result.data.results )
-        this.pagesearch = '1'
+      .then(result => this.movies = result.data.results )
+      this.pagesearch = '1'
     },
+
     getSeries: function() {
       axios.get('https://api.themoviedb.org/3/search/tv', {
               params: {
@@ -43,35 +54,42 @@ var app = new Vue({
                 language: 'it-IT',
               }
     })
-        .then(result => this.series = result.data.results )
+      .then(result => this.series = result.data.results )
     },
+    //funzione per convertire votazione in 5 stelle
     starRating: function(numRating) {
       return Math.ceil(numRating / 2);
     },
+    //funzione per le copertine dei film e serie tv
     setPosterPath: function(path) {
       return `https://image.tmdb.org/t/p/w342/${path}`;
     },
+    //funzione per la sezione "harry potter" nella home
     home1: function() {
-      axios.get(urlmovie, {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
               params: {
                 api_key: '7cef99d6f2b7da5c519fa01d79fc87e1',
                 query: 'harry potter',
                 language: 'it-IT',
               }
     })
-        .then(result => this.moviesHome1 = result.data.results )
-
+      .then(result => this.moviesHome1 = result.data.results )
     },
+    //funzione per la sezione "Il Signore Degli Anelli" nella home
     home2: function() {
-      axios.get(urlmovie, {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
               params: {
                 api_key: '7cef99d6f2b7da5c519fa01d79fc87e1',
                 query: 'il signore degli anelli',
                 language: 'it-IT',
               }
     })
-        .then(result => this.moviesHome2 = result.data.results )
-
+      .then(result => this.moviesHome2 = result.data.results )
     },
+    //funzione per far apparire una finestra al click del film e serie tv
+    showOverlay: function(film) {
+      this.overlayStatus = true;
+    },
+
   }
 });
