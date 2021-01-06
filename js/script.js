e8d23e00ba717ec6b5c1756d5fa8a8f511bd5d81
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2021-01-06 11:14:52 +0100

    aggiunto finestra al click con informazioni aggiuntive e modfiche layout

--- js/script.js
+++ js/script.js
@@ -10,14 +10,10 @@ var app = new Vue({
     queryHome1: 'Harry Potter',
     queryHome2:'Il Signore Degli Anelli',
     query: '',
-    overlayStatus: false,
-    overlay: {
-      poster_path: '',
-      overview: '',
-      original_language: '',
-      vote_average: 0,
-      genre: [],
-    },
+    windowStatus: false,
+    movieSelect: "",
+    moviesGenres: [],
+    moviesCast:[]
   },
 
   mounted: function() {
@@ -41,7 +37,7 @@ var app = new Vue({
                 query: this.searchInput,
                 language: 'it-IT',
               }
-    })
+      })
       .then(result => this.movies = result.data.results )
       this.pagesearch = '1'
     },
@@ -53,7 +49,7 @@ var app = new Vue({
                 query: this.searchInput,
                 language: 'it-IT',
               }
-    })
+      })
       .then(result => this.series = result.data.results )
     },
     //funzione per convertire votazione in 5 stelle
@@ -72,7 +68,7 @@ var app = new Vue({
                 query: 'harry potter',
                 language: 'it-IT',
               }
-    })
+      })
       .then(result => this.moviesHome1 = result.data.results )
     },
     //funzione per la sezione "Il Signore Degli Anelli" nella home
@@ -83,13 +79,72 @@ var app = new Vue({
                 query: 'il signore degli anelli',
                 language: 'it-IT',
               }
-    })
+      })
       .then(result => this.moviesHome2 = result.data.results )
     },
-    //funzione per far apparire una finestra al click del film e serie tv
-    showOverlay: function(film) {
-      this.overlayStatus = true;
+    //funzioni per far apparire al click una finestra con informazioni aggiountive
+    details: function(index) {
+      this.windowStatus = true;
+      if (this.pagesearch === '0') {
+        this.movieSelect = this.moviesHome1[index]
+      } else if (this.pagesearch === '1') {
+        this.movieSelect = this.movies[index]
+      }
     },
+
+    details2: function(index) {
+      this.windowStatus = true;
+      if (this.pagesearch === '0') {
+        this.movieSelect = this.moviesHome2[index];
+      } else if (this.pagesearch === '1') {
+        this.movieSelect = this.series[index]
+      }
+    },
+    //funzione per estrarre i generi dei film
+    movieGen: function (idFilm) {
+      this.moviesGenres = []
+      axios.get('https://api.themoviedb.org/3/movie/' + idFilm + '?api_key=7cef99d6f2b7da5c519fa01d79fc87e1&language=it-IT')
+      .then(response => {
+        let gen = response.data.genres
+        for (var i = 0; i < gen.length; i++) {
+          this.moviesGenres.push(gen[i].name);
+        }
+      })
+    },
+    //funzione per estrarre i generi delle serie tv
+    serieGen: function (idTv) {
+      this.moviesGenres = []
+      axios.get('https://api.themoviedb.org/3/tv/' + idTv + '?api_key=7cef99d6f2b7da5c519fa01d79fc87e1&language=it-IT')
+      .then(response => {
+        let gen = response.data.genres
+        for (var i = 0; i < gen.length; i++) {
+          this.moviesGenres.push(gen[i].name);
+
+        }
+      })
+    },
+    //funzione per richiamare 5 attori del cast (film)
+    movieCast: function (idFilm) {
+      this.moviesCast = []
+      axios.get('https://api.themoviedb.org/3/movie/' + idFilm + '/credits?api_key=7cef99d6f2b7da5c519fa01d79fc87e1&language=it-IT')
+      .then(response => {
+        let actor = response.data.cast
+        for (var i = 0; i < 5; i++) {
+          this.moviesCast.push(actor[i].name);
+        }
+      })
+    },
+    //funzione per richiamare 5 attori del cast (serie tv)
+    serieCast: function (idTv) {
+      this.moviesCast = []
+      axios.get('https://api.themoviedb.org/3/tv/' + idTv + '/credits?api_key=7cef99d6f2b7da5c519fa01d79fc87e1&language=it-IT')
+      .then(response => {
+        let actor = response.data.cast
+        for (var i = 0; i < 5; i++) {
+          this.moviesCast.push(actor[i].name);
+        }
+      })
+    }
 
   }
 });
